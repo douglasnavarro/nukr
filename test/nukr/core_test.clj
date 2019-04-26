f01560742fa661123261e261f96deb0072b3a5fc
douglas <douglasnavarro94@gmail.com>
2019-04-26 00:11:34 -0300

    add post test case

--- test/nukr/core_test.clj
+++ test/nukr/core_test.clj
@@ -5,8 +5,10 @@
 
 (deftest home-handler-test
   (testing "home endpoint"
-    (is (= (home (mock/request :get "/"))
-           {:status 200
+    (is (= {:status 200
             :body   "Welcome to Nukr!"
-            :headers {"content-type" "text/plain"}})
-        " returns 200 and greeting message on GET")))
+            :headers {"content-type" "text/plain"}}
+           (app (mock/request :get "/")))
+        " returns 200 and greeting message on GET")
+    (is (= 404 (:status (app (mock/request :post "/"))))
+        " returns 404 on POST")))
