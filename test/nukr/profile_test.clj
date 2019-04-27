af9c7e78293a4932bc2169dfb548365c3ba22b75
douglas <douglasnavarro94@gmail.com>
2019-04-27 17:52:27 -0300

    add add-connection test

--- test/nukr/profile_test.clj
+++ test/nukr/profile_test.clj
@@ -25,11 +25,17 @@
     (is (= #{}      (profile/get-connections a-profile)))
     (is (= false    (profile/hidden? a-profile)))))
 
+(deftest add-connection-test
+  (let [a-profile   (profile/create 1 "Edward")
+        new-profile (profile/add-connection a-profile 2)]
+    (is (= #{2}
+           (profile/get-connections new-profile)))))
+
 (deftest connect-test
-  (let [profile1 (profile/create 1 "Cris")
-        profile2 (profile/create 2 "Ed")
-        profile1-2 {:id 1 :name "Cris" :connections #{2} :hidden false}
-        profile2-1 {:id 2 :name "Ed"   :connections #{1} :hidden false}]
-    (is (= [profile1-2 profile2-1]
+  (let [profile1   (profile/create 1 "Cris")
+        profile2   (profile/create 2 "Ed")
+        new-1      {:id 1 :name "Cris" :connections #{2} :hidden false}
+        new-2      {:id 2 :name "Ed"   :connections #{1} :hidden false}]
+    (is (= [new-1 new-2]
            (profile/connect profile1 profile2))
         "connect func returns connected version of input profiles")))
