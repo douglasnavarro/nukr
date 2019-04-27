94791a373c92e1243a55c4155e9d2e5daeaa61b5
douglas <douglasnavarro94@gmail.com>
2019-04-26 22:52:24 -0300

    rename core_test to integration_test

--- test/nukr/integration_test.clj
+++ test/nukr/integration_test.clj
@@ -1,12 +1,12 @@
-(ns nukr.core-test
+(ns nukr.integration-test
   (:require [clojure.test :refer :all]
             [nukr.core :refer :all]
             [ring.mock.request :as mock]))
 
 (deftest home-handler-test
   (testing "home endpoint"
-    (is (= {:status 200
-            :body   "Welcome to Nukr!"
+    (is (= {:status  200
+            :body    "Welcome to Nukr!"
             :headers {"content-type" "text/plain"}}
            (app (mock/request :get "/")))
         " returns 200 and greeting message on GET")
