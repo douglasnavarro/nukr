56a6b9ef363f1a65ec63d391edcf642e039d36f7
douglas <douglasnavarro94@gmail.com>
2019-04-28 16:12:44 -0300

    improve profile_view

--- src/nukr/profile_view.clj
+++ src/nukr/profile_view.clj
@@ -1,5 +1,5 @@
 (ns nukr.profile-view
-  "Views. Provides declarative html generation for handlers."
+  "Views. Provides dynamic declarative html generation for handlers."
   (require [hiccup.page :refer [html5]]))
 
 (defn- create-profile-form []
@@ -11,6 +11,15 @@
             [:input.filled-in {:type "checkbox" :name "hidden"}]
             [:span "I don't want suggestions"]]]
           [:button {:type "submit" :class "btn purple"} "Join"]]))
+
+(defn create-card [profile]
+  (html5 [:div.card
+          [:div.card-image
+           [:img {:src "/placeholders/avatar.jpg"}]]
+                  ; :style "max-width:200px;"}]]
+          [:div.card-content
+           [:span.card-title (:name profile)]]]))
+
 
 (defn profiles-page [profiles]
   (html5 {:lang :en}
@@ -28,7 +37,12 @@
               [:h5 "The future is purple - and connected!"]
               [:hr]]]
            [:div.row {:id "profiles-list"}
-            [:p profiles]]
+            (if (seq profiles)
+              (for [profile profiles]
+                [:div {:class "col l3 m5 s12"}
+                 (create-card profile)])
+              [:div.col
+               [:p "No profiles. Lead the way!"]])]
            [:div.row {:id "create-profile"}
             [:div {:class "col s4 push-s4 center-align"}
              (create-profile-form)]]]
