817c218a64f7c808d24b7d97f9b87d4465dc2e81
douglas <douglasnavarro94@gmail.com>
2019-05-04 00:01:55 -0300

    reduce tests coupling

--- test/nukr/profile_model_test.clj
+++ test/nukr/profile_model_test.clj
@@ -3,34 +3,27 @@
             [nukr.profile-logic :as logic]
             [nukr.profile-state :as state]))
 
+(def cris-mock {:name "Cris" :connections #{} :hidden false})
+(def ed-mock {:name "Ed" :connections #{} :hidden false})
+
 (deftest add-profile-test
-  (let [storage (state/create-profile-storage!)
-        cris (logic/create "Cris")
-        ed (logic/create "Ed")
-        _ (state/add-profile! cris storage)]
+  (let [storage (ref #{})
+        _ (state/add-profile! cris-mock storage)]
    (is (= #{{:name "Cris" :connections #{} :hidden false}}
           @storage))
    (is (= #{{:name "Cris" :connections #{} :hidden false}
             {:name "Ed" :connections #{} :hidden false}}
-         (do (state/add-profile! ed storage))))))
+         (do (state/add-profile! ed-mock storage))))))
 
 (deftest remove-profile-test
-  (let [storage (state/create-profile-storage!)
-        cris (logic/create "Cris")
-        ed (logic/create "Ed")
-        _ (state/add-profile! cris storage)
-        _ (state/add-profile! ed storage)]
+  (let [storage (ref #{cris-mock ed-mock})]
    (is (= #{{:name "Cris" :connections #{} :hidden false}}
           (do (state/remove-profile! "Ed" storage))))
    (is (= #{}
           (do (state/remove-profile! "Cris" storage))))))
 
 (deftest connect-profiles-test
-  (let [storage (state/create-profile-storage!)
-        cris (logic/create "Cris")
-        ed (logic/create "Ed")
-        _ (state/add-profile! cris storage)
-        _ (state/add-profile! ed storage)]
+  (let [storage (ref #{cris-mock ed-mock})]
    (is (= #{{:name "Cris" :connections #{"Ed"} :hidden false}
             {:name "Ed" :connections #{"Cris"} :hidden false}}
           (do (state/connect-profiles! "Cris" "Ed" storage))))))
@@ -39,26 +32,26 @@
   (let [cris  {:name "Cris" :connections #{"David"} :hidden false}
         ed    {:name "Ed" :connections #{"David"} :hidden false}
         david {:name "David" :connections #{"Cris" "Ed"} :hidden false}
-        profiles (ref #{cris ed david})]
+        storage (ref #{cris ed david})]
     (is (= '({:target "Ed" :relevance 1})
-           (state/get-suggestions cris @profiles))
+           (state/get-suggestions cris @storage))
         "get-suggestion sets target and relevance correctly")
     (is (= '({:target "Cris" :relevance 1})
-           (state/get-suggestions ed @profiles))
+           (state/get-suggestions ed @storage))
         "get-suggestion sets target and relevance correctly")
     (is (= '()
-           (state/get-suggestions david @profiles))
+           (state/get-suggestions david @storage))
         "no suggestion if already connected to everyone")))
 
 (deftest get-suggestions-test-with-hidden
   (let [cris  {:name "Cris" :connections #{"David"} :hidden false}
         ed    {:name "Ed" :connections #{"David"} :hidden true}
         david {:name "David" :connections #{"Cris" "Ed"} :hidden false}
-        profiles (ref #{cris ed david})]
+        storage (ref #{cris ed david})]
     (is (= '()
-           (state/get-suggestions cris @profiles))
+           (state/get-suggestions cris @storage))
         "hidden profile must not be shown in suggestion")
     (is (= '({:target "Cris" :relevance 1})
-           (state/get-suggestions ed @profiles)))
+           (state/get-suggestions ed @storage)))
     (is (= '()
-           (state/get-suggestions david @profiles)))))
+           (state/get-suggestions david @storage)))))
